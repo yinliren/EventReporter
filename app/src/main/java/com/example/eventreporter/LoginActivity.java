96f5aac18118ced3b8c7dd0c98b2dfa99974167a
liren <yinliren@gmail.com>
2019-06-20 23:16:59 -0700

    Successfully connect to Firebase!

---
+++ app/src/main/java/com/example/eventreporter/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.eventreporter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello, Liren! I am version2 with github.");
+    }
+}
