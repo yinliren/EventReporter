7325d7446cf5f63f98ea11bee4c85dafcfdf1a0c
liren <yinliren@gmail.com>
2019-06-24 08:18:52 -0700

    added comment activity added showing comment page after tapping the event on the list

--- app/src/main/java/com/example/eventreporter/Event.java
+++ app/src/main/java/com/example/eventreporter/Event.java
@@ -1,27 +1,47 @@
 package com.example.eventreporter;
 
 public class Event {
-
     /**
      * All data for a event.
      */
     private String title;
     private String address;
     private String description;
-
-
     private int like;
     private String id;
     private long time;
     private String username;
     private String imgUri;
+    private int CommentNumber;
+    private double latitude;
+    private double longitude;
+
+    public int getCommentNumber() {
+        return CommentNumber;
+    }
+
+    public void setCommentNumber(int commentNumber) {
+        CommentNumber = commentNumber;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 
     public void setTitle(String title) {
         this.title = title;
     }
 
+    public String getAddress() {
+        return address;
+    }
+
     public void setAddress(String address) {
         this.address = address;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public void setDescription(String description) {
@@ -68,31 +88,27 @@ public class Event {
         this.imgUri = imgUri;
     }
 
-    public Event() {
-    }
+    public Event() {}
 
-    /**
-     * Constructor
-     */
     public Event(String title, String address, String description) {
         this.title = title;
-
         this.address = address;
         this.description = description;
     }
 
-    /**
-     * Getters for private attributes of Event class.
-     */
-    public String getTitle() {
-        return this.title;
+    public double getLatitude() {
+        return latitude;
     }
 
-    public String getAddress() {
-        return this.address;
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
     }
 
-    public String getDescription() {
-        return this.description;
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
     }
 }
